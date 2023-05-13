929a487eb9c0d63f7fed99558fbaa763b6fac9bf
CherenTheCat <Dark0flare@gmail.com>
2023-05-12 22:42:50 -0500

    make armor and elytra visible

--- scripts/hideavatar.lua
+++ scripts/hideavatar.lua
@@ -1,2 +1,4 @@
 vanilla_model.ALL:setVisible(false)
 vanilla_model.HELD_ITEMS:setVisible(true) -- Held Items
+vanilla_model.ELYTRA:setVisible(true) -- Elytra
+vanilla_model.ARMOR:setVisible(true) -- Elytra
